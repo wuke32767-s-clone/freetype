740202fbef8790f14b18110c448d3d1dff60ab8d
wuke32767 <2446214230@qq.com>
2025-06-18 11:57:28 +0800

    apply https://github.com/Robmaister/SharpFont.Dependencies/raw/master/freetype2/win64.patch

--- include/freetype/fttypes.h
+++ include/freetype/fttypes.h
@@ -242,7 +242,11 @@ FT_BEGIN_HEADER
    * @description:
    *   A typedef for signed long.
    */
+#if _WIN64
+  typedef signed __int64  FT_Long;
+#else
   typedef signed long  FT_Long;
+#endif
 
 
   /**************************************************************************
@@ -253,7 +257,11 @@ FT_BEGIN_HEADER
    * @description:
    *   A typedef for unsigned long.
    */
+#if _WIN64
+   typedef unsigned __int64  FT_ULong;
+#else
   typedef unsigned long  FT_ULong;
+#endif
 
 
   /**************************************************************************
@@ -275,7 +283,11 @@ FT_BEGIN_HEADER
    * @description:
    *   A signed 26.6 fixed-point type used for vectorial pixel coordinates.
    */
+#if _WIN64
+  typedef signed __int64  FT_F26Dot6;
+#else
   typedef signed long  FT_F26Dot6;
+#endif
 
 
   /**************************************************************************
@@ -287,7 +299,11 @@ FT_BEGIN_HEADER
    *   This type is used to store 16.16 fixed-point values, like scaling
    *   values or matrix coefficients.
    */
+#if _WIN64
+  typedef signed __int64  FT_Fixed;
+#else
   typedef signed long  FT_Fixed;
+#endif
 
 
   /**************************************************************************
